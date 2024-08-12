9a7af5c0cf415aeb7bccd5b13e895788d6025119
Clairevf <clairevanfossen@clearchannel.com>
2024-08-12 09:33:50 -0700

    move SpecificStartTime into DigitalDetails

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes in io-sdk-golang will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.3.12] - 2024-08-12
+
+- update booking DigitalDetails model
+
 ## [0.3.11] - 2024-08-08
 
 - Remove structure-api since it is being deprecated
--- pkg/bookings/models.go
+++ pkg/bookings/models.go
@@ -3,19 +3,18 @@ package bookings
 import "time"
 
 type Booking struct {
-	Canceled          *bool           `json:"canceled,omitempty" bson:"canceled,omitempty"`
-	CreatedAt         time.Time       `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	Digital           *DigitalDetails `json:"digital,omitempty" bson:"digital,omitempty"`
-	EndDate           *time.Time      `json:"endDate,omitempty" bson:"endDate,omitempty"`
-	ExternalIDs       []string        `json:"externalIDs,omitempty" bson:"externalIDs,omitempty"`
-	Filler            bool            `json:"filler,omitempty" bson:"filler,omitempty"`
-	ID                string          `json:"id,omitempty" bson:"id,omitempty"`
-	OrderLineID       string          `json:"orderLineID,omitempty" bson:"orderLineID,omitempty"`
-	Print             *PrintDetails   `json:"print,omitempty" bson:"print,omitempty"`
-	SpecificStartTime string          `json:"specificStartTime,omitempty" bson:"specificStartTime,omitempty"`
-	StartDate         *time.Time      `json:"startDate,omitempty" bson:"startDate,omitempty"`
-	UpdatedAt         time.Time       `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
-	Waitlisted        *bool           `json:"waitlisted,omitempty" bson:"waitlisted,omitempty"`
+	Canceled    *bool           `json:"canceled,omitempty" bson:"canceled,omitempty"`
+	CreatedAt   time.Time       `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	Digital     *DigitalDetails `json:"digital,omitempty" bson:"digital,omitempty"`
+	EndDate     *time.Time      `json:"endDate,omitempty" bson:"endDate,omitempty"`
+	ExternalIDs []string        `json:"externalIDs,omitempty" bson:"externalIDs,omitempty"`
+	Filler      bool            `json:"filler,omitempty" bson:"filler,omitempty"`
+	ID          string          `json:"id,omitempty" bson:"id,omitempty"`
+	OrderLineID string          `json:"orderLineID,omitempty" bson:"orderLineID,omitempty"`
+	Print       *PrintDetails   `json:"print,omitempty" bson:"print,omitempty"`
+	StartDate   *time.Time      `json:"startDate,omitempty" bson:"startDate,omitempty"`
+	UpdatedAt   time.Time       `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+	Waitlisted  *bool           `json:"waitlisted,omitempty" bson:"waitlisted,omitempty"`
 }
 
 type PrintDetails struct {
@@ -24,14 +23,15 @@ type PrintDetails struct {
 }
 
 type DigitalDetails struct {
-	DailyEndTime   string             `json:"dailyEndTime,omitempty" bson:"dailyEndTime,omitempty"`
-	DailyStartTime string             `json:"dailyStartTime,omitempty" bson:"dailyStartTime,omitempty"`
-	DaysToPlay     *DigitalDaysToPlay `json:"daysToPlay,omitempty" bson:"daysToPlay,omitempty"`
-	Frequency      int                `json:"frequency,omitempty" bson:"frequency,omitempty"`
-	NetworkID      string             `json:"networkID,omitempty" bson:"networkID,omitempty"`
-	NumberOfSlots  int                `json:"numberOfSlots,omitempty" bson:"numberOfSlots,omitempty"`
-	SlotSeconds    float32            `json:"slotSeconds,omitempty" bson:"slotSeconds,omitempty"`
-	SlotSlices     int                `json:"slotSlices,omitempty" bson:"slotSlices,omitempty"`
+	DailyEndTime      string             `json:"dailyEndTime,omitempty" bson:"dailyEndTime,omitempty"`
+	DailyStartTime    string             `json:"dailyStartTime,omitempty" bson:"dailyStartTime,omitempty"`
+	DaysToPlay        *DigitalDaysToPlay `json:"daysToPlay,omitempty" bson:"daysToPlay,omitempty"`
+	Frequency         int                `json:"frequency,omitempty" bson:"frequency,omitempty"`
+	NetworkID         string             `json:"networkID,omitempty" bson:"networkID,omitempty"`
+	NumberOfSlots     int                `json:"numberOfSlots,omitempty" bson:"numberOfSlots,omitempty"`
+	SlotSeconds       float32            `json:"slotSeconds,omitempty" bson:"slotSeconds,omitempty"`
+	SlotSlices        int                `json:"slotSlices,omitempty" bson:"slotSlices,omitempty"`
+	SpecificStartTime string             `json:"specificStartTime,omitempty" bson:"specificStartTime,omitempty"`
 }
 
 type DigitalDaysToPlay struct {
