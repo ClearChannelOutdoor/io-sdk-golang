ed137f02df983cc2d8780c47030bb42533ea0440
Clairevf <clairevanfossen@clearchannel.com>
2024-10-29 09:24:37 -0700

    update order model

--- pkg/orders/orders.go
+++ pkg/orders/orders.go
@@ -12,7 +12,7 @@ const (
 	serverUrl    string = "https://direct.cco.io"
 )
 
-func NewOrdersClient(ctx context.Context, oauth2 *clientcredentials.Config, overrideSvr ...string) (*clients.Client[Order], error) {
+func NewClient(ctx context.Context, oauth2 *clientcredentials.Config, overrideSvr ...string) (*clients.Client[Order], error) {
 	svr := serverUrl
 	if len(overrideSvr) > 0 && overrideSvr[0] != "" {
 		svr = overrideSvr[0]
