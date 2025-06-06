7db54410adfe01995e9171568229e127b7aff4c9
Robin Momii <robinmomii@clearchannel.com>
2025-06-06 11:48:43 -0700

    updated booking model to contain quantity buys

--- pkg/bookings/models.go
+++ pkg/bookings/models.go
@@ -5,23 +5,24 @@ import (
 )
 
 type Booking struct {
-	ID           string          `json:"bookingID,omitempty"`
-	BuyType      *BuyType        `json:"buyType,omitempty"`
-	Cost         *float32        `json:"cost,omitempty"`
-	CreatedAt    *time.Time      `json:"createdAt,omitempty"`
-	DeletedAt    *time.Time      `json:"deletedAt,omitempty"`
-	Digital      *DigitalDetails `json:"digital,omitempty"`
-	EndDate      *time.Time      `json:"endDate,omitempty"`
-	ExternalIDs  []string        `json:"externalIDs,omitempty"`
-	Filler       bool            `json:"filler,omitempty"`
-	MarketID     string          `json:"marketID,omitempty"`
-	MediaProduct *MediaProduct   `json:"mediaProduct,omitempty"`
-	OrderID      string          `json:"orderID,omitempty"`
-	Print        *PrintDetails   `json:"print,omitempty"`
-	StartDate    *time.Time      `json:"startDate,omitempty"`
-	Status       *BookingStatus  `json:"status,omitempty"`
-	UpdatedAt    *time.Time      `json:"updatedAt,omitempty"`
-	Waitlisted   *bool           `json:"waitlisted,omitempty"`
+	ID           string           `json:"bookingID"`
+	BuyType      *BuyType         `json:"buyType,omitempty"`
+	Cost         *float32         `json:"cost,omitempty"`
+	CreatedAt    *time.Time       `json:"createdAt"`
+	DeletedAt    *time.Time       `json:"deletedAt,omitempty"`
+	Display      *DisplayDetails  `json:"display,omitempty"`
+	EndDate      *time.Time       `json:"endDate,omitempty"`
+	ExternalIDs  []string         `json:"externalIDs,omitempty"`
+	Filler       bool             `json:"filler,omitempty"`
+	Market       *Market          `json:"market,omitempty"`
+	MediaProduct *MediaProduct    `json:"mediaProduct,omitempty"`
+	Network      *NetworkDetails  `json:"network,omitempty"`
+	OrderID      string           `json:"orderID,omitempty"`
+	Quantity     *QuantityDetails `json:"quantity,omitempty"`
+	StartDate    *time.Time       `json:"startDate,omitempty"`
+	Status       *BookingStatus   `json:"status,omitempty"`
+	UpdatedAt    *time.Time       `json:"updatedAt"`
+	Waitlisted   *bool            `json:"waitlisted,omitempty"`
 }
 type BookingStatus struct {
 	Status BookingStatusValue   `json:"status,omitempty"`
@@ -34,18 +35,6 @@ type BuyType struct {
 	RevenueSpecifier RevenueSpecifier `json:"revenueSpecifier,omitempty"`
 }
 
-type DigitalDetails struct {
-	NetworkID         string             `json:"networkID,omitempty"`
-	DailyStartTime    string             `json:"dailyStartTime,omitempty"`
-	DailyEndTime      string             `json:"dailyEndTime,omitempty"`
-	DaysToPlay        *DigitalDaysToPlay `json:"daysToPlay,omitempty"`
-	NumberOfSlots     int                `json:"numberOfSlots,omitempty"`
-	Frequency         int                `json:"frequency,omitempty"`
-	SlotSeconds       float32            `json:"slotSeconds,omitempty"`
-	SlotSlices        int                `json:"slotSlices,omitempty"`
-	SpecificStartTime string             `json:"specificStartTime,omitempty"`
-}
-
 type DigitalDaysToPlay struct {
 	Sunday    bool `json:"sunday"`
 	Monday    bool `json:"monday"`
@@ -55,14 +44,49 @@ type DigitalDaysToPlay struct {
 	Friday    bool `json:"friday"`
 	Saturday  bool `json:"saturday"`
 }
+
+type DisplayDetails struct {
+	DisplayID   string   `json:"displayID,omitempty"`
+	ExternalIDs []string `json:"externalIDs,omitempty"`
+	Slots       int      `json:"slots,omitempty"`
+}
+
+type Market struct {
+	Code        string   `json:"code,omitempty"`
+	ExternalIDs []string `json:"externalIDs,omitempty"`
+	MarketID    string   `json:"marketID,omitempty"`
+}
+
 type MediaProduct struct {
 	ProductCode string `json:"productCode,omitempty"`
 	TypeCode    string `json:"typeCode,omitempty"`
 }
 
-type PrintDetails struct {
-	DisplayID   string   `json:"displayID,omitempty"`
-	ExternalIDs []string `json:"externalIDs,omitempty"`
+type NetworkDetails struct {
+	DailyEndTime      string             `json:"dailyEndTime,omitempty"`
+	DailyStartTime    string             `json:"dailyStartTime,omitempty"`
+	DaysToPlay        *DigitalDaysToPlay `json:"daysToPlay,omitempty"`
+	Frequency         int                `json:"frequency,omitempty"`
+	NetworkID         string             `json:"networkID,omitempty"`
+	NumberOfSlots     int                `json:"numberOfSlots,omitempty"`
+	SlotSeconds       float32            `json:"slotSeconds,omitempty"`
+	SlotSlices        int                `json:"slotSlices,omitempty"`
+	SpecificStartTime string             `json:"specificStartTime,omitempty"`
+}
+
+type QuantityCustomDetails struct {
+	ExternalIDs []string          `json:"externalIDs,omitempty"`
+	Displays    []*DisplayDetails `json:"displays,omitempty"`
+}
+
+type QuantityDetails struct {
+	Custom            *QuantityCustomDetails     `json:"custom,omitempty"`
+	FullMarket        *QuantityFullMarketDetails `json:"fullMarket,omitempty"`
+	RequestedQuantity int                        `json:"requestedQuantity,omitempty"`
+}
+
+type QuantityFullMarketDetails struct {
+	Weight int `json:"weight,omitempty"`
 }
 
 const ExplicitEmpty = ""
@@ -91,7 +115,7 @@ type Flexibility string
 const (
 	Fixed            Flexibility = "Fixed"
 	Flexible         Flexibility = "Flexible"
-	EmptyFlexibility Flexibility = ExplicitEmpty // for bonus override only
+	EmptyFlexibility Flexibility = ExplicitEmpty
 )
 
 type RevenueSpecifier string
@@ -102,7 +126,7 @@ const (
 	FBI                   RevenueSpecifier = "FBI"
 	Lessor                RevenueSpecifier = "Lessor"
 	MakeGood              RevenueSpecifier = "Make Good"
-	PSA                   RevenueSpecifier = "PSA" // lives on order, not booking, may still come through on an orderline message and will need to be parsed correctly
+	PSA                   RevenueSpecifier = "PSA"
 	RFR                   RevenueSpecifier = "Right of First Refusal"
 	Trade                 RevenueSpecifier = "Trade"
 )
