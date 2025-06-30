b54e7598533cbdc1ddf356482f99a15655b54d32
Robin Momii <robinmomii@clearchannel.com>
2025-06-30 14:53:10 -0700

    updated order model

--- pkg/orders/models.go
+++ pkg/orders/models.go
@@ -2,50 +2,49 @@ package orders
 
 import "time"
 
-/* Orders */
 type Order struct {
 	AdvertisedProductCode   string         `json:"advertisedProductCode,omitempty"`
 	Advertiser              CustomerEntity `json:"advertiser,omitempty"`
 	Buyer                   CustomerEntity `json:"buyer,omitempty"`
-	Canceled                *bool          `json:"canceled"`
-	CancellationTerms       string         `json:"cancellationTerms,omitempty"`
-	CancellationReason      string         `json:"cancellationReason,omitempty" `
+	Canceled                *bool          `json:"canceled,omitempty"`
 	CreatedAt               time.Time      `json:"createdAt,omitempty"`
 	CreativeChangeAgreement *bool          `json:"creativeChangeAgreement,omitempty"`
 	EndDate                 time.Time      `json:"endDate,omitempty"`
+	ExternalIDs             []string       `json:"externalIDs,omitempty"`
 	ID                      string         `json:"orderID,omitempty"`
-	Markets                 []OrderMarket  `json:"markets,omitempty" `
-	Number                  string         `json:"number,omitempty"`
+	Markets                 []OrderMarket  `json:"markets,omitempty"`
 	Name                    string         `json:"name,omitempty"`
+	Number                  string         `json:"number,omitempty"`
 	Psa                     *bool          `json:"psa,omitempty"`
 	Seller                  *OrderSeller   `json:"seller,omitempty"`
 	Source                  OrderSource    `json:"source,omitempty"`
 	StartDate               time.Time      `json:"startDate,omitempty"`
 	UpdatedAt               time.Time      `json:"updatedAt,omitempty"`
+}
 
+type CustomerEntity struct {
+	ID           string `json:"customerID,omitempty"`
+	IsAdvertiser bool   `json:"isAdvertiser,omitempty"`
+	Number       string `json:"number,omitempty"`
+}
+
+type Employee struct {
 	ExternalIDs []string `json:"externalIDs,omitempty"`
+	Number      string   `json:"number,omitempty"`
 }
 
 type OrderMarket struct {
 	Canceled           bool               `json:"canceled,omitempty"`
-	MarketID           string             `json:"marketID,omitempty"`
-	MarketCode         string             `json:"marketCode,omitempty"`
-	SubmissionCategory SubmissionCategory `json:"submissionCategory,omitempty"`
+	CancellationReason string             `json:"cancellationReason,omitempty"`
+	CancellationTerms  string             `json:"cancellationTerms,omitempty"`
 	ExternalIDs        []string           `json:"externalIDs,omitempty"`
-}
-type Employee struct {
-	Number      string   `json:"number,omitempty"`
-	ExternalIDs []string `json:"externalIDs,omitempty"`
+	MarketCode         string             `json:"marketCode,omitempty"`
+	MarketID           string             `json:"marketID,omitempty"`
+	SubmissionCategory SubmissionCategory `json:"submissionCategory,omitempty"`
 }
 
 type OrderSeller struct {
 	LeadAccountExec         Employee `json:"leadAccountExec,omitempty"`
 	SellingBusinessUnitCode string   `json:"sellingBusinessUnitCode,omitempty"`
 	SupportingAccountExec   Employee `json:"supportingAccountExec,omitempty"`
-}
-
-type CustomerEntity struct {
-	ID           string `json:"customerID"`
-	Number       string `json:"number,omitempty"`
-	IsAdvertiser bool   `json:"isAdvertiser,omitempty"`
 }
