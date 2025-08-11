8151fbcb1280d0fae8789e99d76500bf9790c780
Robin Momii <robinmomii@clearchannel.com>
2025-08-11 12:16:38 -0700

    updated the order model to have names for customers and employees

--- pkg/orders/models.go
+++ pkg/orders/models.go
@@ -3,34 +3,36 @@ package orders
 import "time"
 
 type Order struct {
-	AdvertisedProductCode   string         `json:"advertisedProductCode,omitempty"`
-	Advertiser              CustomerEntity `json:"advertiser,omitempty"`
-	Buyer                   CustomerEntity `json:"buyer,omitempty"`
-	Canceled                *bool          `json:"canceled,omitempty"`
-	CancellationTerms       string         `json:"cancellationTerms,omitempty"`
-	CreatedAt               time.Time      `json:"createdAt,omitempty"`
-	CreativeChangeAgreement *bool          `json:"creativeChangeAgreement,omitempty"`
-	EndDate                 time.Time      `json:"endDate,omitempty"`
-	ExternalIDs             []string       `json:"externalIDs,omitempty"`
-	ID                      string         `json:"orderID,omitempty"`
-	Markets                 []OrderMarket  `json:"markets,omitempty"`
-	Name                    string         `json:"name,omitempty"`
-	Number                  string         `json:"number,omitempty"`
-	Psa                     *bool          `json:"psa,omitempty"`
-	Seller                  *OrderSeller   `json:"seller,omitempty"`
-	Source                  OrderSource    `json:"source,omitempty"`
-	StartDate               time.Time      `json:"startDate,omitempty"`
-	UpdatedAt               time.Time      `json:"updatedAt,omitempty"`
+	AdvertisedProductCode   string          `json:"advertisedProductCode,omitempty"`
+	Advertiser              *CustomerEntity `json:"advertiser,omitempty"`
+	Buyer                   *CustomerEntity `json:"buyer,omitempty"`
+	Canceled                *bool           `json:"canceled,omitempty"`
+	CancellationTerms       string          `json:"cancellationTerms,omitempty"`
+	CreatedAt               time.Time       `json:"createdAt,omitempty"`
+	CreativeChangeAgreement *bool           `json:"creativeChangeAgreement,omitempty"`
+	EndDate                 time.Time       `json:"endDate,omitempty"`
+	ExternalIDs             []string        `json:"externalIDs,omitempty"`
+	ID                      string          `json:"orderID,omitempty"`
+	Markets                 []OrderMarket   `json:"markets,omitempty"`
+	Name                    string          `json:"name,omitempty"`
+	Number                  string          `json:"number,omitempty"`
+	Psa                     *bool           `json:"psa,omitempty"`
+	Seller                  *OrderSeller    `json:"seller,omitempty"`
+	Source                  OrderSource     `json:"source,omitempty"`
+	StartDate               time.Time       `json:"startDate,omitempty"`
+	UpdatedAt               time.Time       `json:"updatedAt,omitempty"`
 }
 
 type CustomerEntity struct {
 	ID           string `json:"customerID,omitempty"`
 	IsAdvertiser bool   `json:"isAdvertiser,omitempty"`
+	Name         string `json:"name,omitempty"`
 	Number       string `json:"number,omitempty"`
 }
 
 type Employee struct {
 	ExternalIDs []string `json:"externalIDs,omitempty"`
+	Name        string   `json:"name,omitempty"`
 	Number      string   `json:"number,omitempty"`
 }
 
@@ -44,7 +46,7 @@ type OrderMarket struct {
 }
 
 type OrderSeller struct {
-	LeadAccountExec         Employee `json:"leadAccountExec,omitempty"`
-	SellingBusinessUnitCode string   `json:"sellingBusinessUnitCode,omitempty"`
-	SupportingAccountExec   Employee `json:"supportingAccountExec,omitempty"`
+	LeadAccountExec         *Employee `json:"leadAccountExec,omitempty"`
+	SellingBusinessUnitCode string    `json:"sellingBusinessUnitCode,omitempty"`
+	SupportingAccountExec   *Employee `json:"supportingAccountExec,omitempty"`
 }
